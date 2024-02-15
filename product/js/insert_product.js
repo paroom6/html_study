f66d2bb189577961ae60c007fadc8828d456a145
paroom6 <opwqer3729@gmail.com>
2024-02-15 17:49:50 +0900

    feat: 0215 수업자료

--- product/js/insert_product.js
+++ product/js/insert_product.js
@@ -24,28 +24,35 @@
  * id,name,price,size(ss,s,m,l,xl,xxl)
  * 
  * CRDU
+ * 
+ * 이클립스 폴더 생성 -> 필터 -> 서블릿 insert ->  
+ * 
  * */
 async function handleAddClick() {
+    const productInputs = document.querySelectorAll(".product-input");
+    let productObj = {
+        product_name : productInputs[0].value,
+        product_price : parseInt(productInputs[1].value),
+        product_size : productInputs[2].value
+    };
     try {   
-        const productInputs = document.querySelectorAll(".product-input");
-        let productObj = {
-            product_name : productInputs[0].value,
-            product_price : productInputs[1].value,
-            product_size : productInputs[2].value
-        };
-        const product = await fetch("http://localhost:8080/Product/product/addition", {
+        const response = await fetch("http://localhost:8080/Product/product/addition", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(productObj) 
         });
-        if(!product.ok) {
-            throw await product.json();
+        if(!response.ok) {
+            throw await response.json();
         }
+        const responseData = await response.json();
+        console.log(responseData);
+        alert(`${responseData.data.succesCount0}건의 상품 추가 완료`);
         handleSearchClick();
     } catch (error) {
         console.log("에러발생");
-        console.log(error.errorMasseage);
+        alert(error?.errorMasseage);
+        //?. null이나 undifined일 경우 참조하지 않는다.
     }
 }
--- product/js/search_product.js
+++ product/js/search_product.js
@@ -4,26 +4,27 @@
 //   });
 async function handleSearchClick() {
     try {   
-        const JsonProducts = await fetch("http://localhost:8080/Product/product/list");
-        if(!JsonProducts.ok) {
-           throw await JsonProducts.json();
-        }
-        const Products = await JsonProducts.json();
-        console.log(Products);
+        const response = await fetch("http://localhost:8080/Product/product/list");
+        const responseData = await response.json();
+        console.log(responseData);
         const tbody = document.querySelector(".product-body");
         tbody.innerHTML = "";
-        for (const product of Products.data) {
-            tbody.innerHTML  += `
-                <tr>
-                    <td>${product.product_id}</td>
-                    <td>${product.product_name}</td>
-                    <td>${product.product_price}</td>
-                    <td>${product.product_size}</td>
-                </tr>
-            `
+        for (const product of responseData) {
+            tbody.innerHTML += productInfoTr(product);
         }
     } catch (error) {
         console.log(error);
     }
 
+}
+
+function productInfoTr({product_id, product_name, product_price, product_size}) {
+    return `
+        <tr>
+            <td>${product_id}</td>
+            <td>${product_name}</td>
+            <td>${product_price}</td>
+            <td>${product_size}</td>
+        </tr>
+    `
 }
